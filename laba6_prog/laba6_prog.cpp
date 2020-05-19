71de78bcb2b63e6ffaa85afd61a44a46c9115698
herusanna <61598874+herusanna@users.noreply.github.com>
2020-05-19 19:37:07 +0300

    изменила

--- laba6_prog/laba6_prog.cpp
+++ laba6_prog/laba6_prog.cpp
@@ -1,101 +1,133 @@
 ﻿#include <iostream> 
 #include <fstream>
+#include <string>
+#include <ctime>
 using namespace std;
+#pragma warning(disable : 4996)
 struct firm {
-    char firmName[32];
+    string firmName;
     int compSize;
     int compPrice;
-void setInfo(){
-    ofstream fileStrmOut("file.bin", ios::binary);
- cin.ignore(cin.rdbuf()->in_avail());
-    cout << endl << "Firm name : ";
-    cin.getline(firmName, 32);
-    fileStrmOut.write(firmName, sizeof(int));
-    while (!(cin >> compSize))
-    {
-        cout << "Wrong input" << endl;
-        cin.clear();
-        cin.ignore(65535, '\n');
-    }
-    fileStrmOut.write((char*)&compSize, sizeof(int));
-    while (!(cin >> compPrice))
-    {
-        cout << "Wrong input" << endl;
-        cin.clear();
-        cin.ignore(65535, '\n');
-    }
-    fileStrmOut.write((char*)&compPrice, sizeof(int));
-}
-void showInfo() {
-    ofstream fileStrmOut("file.bin", ios::binary);
-    fileStrmOut.write(firmName, sizeof(int));
-    fileStrmOut.write((char*)&compSize, sizeof(int));
-    fileStrmOut.write((char*)&compPrice, sizeof(int));
-    fileStrmOut.close();
-}
-};
-void searchInfo(firm*Comp, int* size ) {
-    int sum=0;
-    double ave;
-    for (size_t i = 0; i < *size; i++)
-    {
-        if ((Comp + i)->compSize >= 19) {
-            (Comp + i)->showInfo();
-            sum += (Comp + i)->compPrice;
+    void setName() {
+        switch (rand() % 6)
+        {
+        case 0: firmName = "Samsung";
+            break;
+        case 1: firmName = "LG";
+            break;
+        case 2: firmName = "Philips";
+            break;
+        case 3: firmName = "AOC";
+            break;
+        case 4: firmName = "Acer";
+            break;
+        case 5: firmName = "Asus";
+            break;
         }
     }
-    for (size_t i = 0; i < *size; i++)
-    {
-         ave = sum / *size;
+    void setInfo() {
+        setName();
+        compSize = rand() % 35 + 16;
+        compPrice = rand() % 30000 + 2000;
     }
-    ofstream fileStrmOut("file.bin", ios::binary);
-    fileStrmOut.write((char*)&ave, sizeof(int));
-}
-
-/*создать бинарный файл с информацией о предложенных к реализации мониторахЖ
-- название фирмы;
-- размер по диагонали;
-- стоимость;
-Определить среднюю цену мониторов, размером не менее 19 дюймов и распечатать сведения о них.*/
-
-void lvl1() {
+    void saveInfo(FILE* file) {
+        fwrite(&firmName, sizeof(string), 1, file);
+        fwrite(&compSize, sizeof(short), 1, file);
+        fwrite(&compPrice, sizeof(int), 1, file);
+    }
+    void outInfo() {
+        cout << firmName<<endl;
+        cout << compPrice << endl;
+        cout << compSize << endl;
+    }
+};
+    void initInfo(firm* Comp, int* size) {
+        for (size_t i = 0; i < *size; i++)
+        {
+            (Comp + i)->setInfo();
+        }
+    }
     
-    int size;
-    cout << "Enter size of products: ";
-    while (!(cin >> size))
+    void showInfo(firm* Comp, int* size, FILE* file)
     {
-        cout << "Wrong input" << endl;
-        cin.clear();
-        cin.ignore(65535, '\n');
+        for (size_t i = 0; i < *size; i++)
+        {
+            (Comp + i)->saveInfo(file);
+        }
     }
-    int* tempSize = &size;
-    firm* Comp = new firm[size];
-    ifstream fileStrmIn("file.bin", ios::binary);
-    searchInfo(Comp, tempSize);
-    //while (fileStrmIn.read((char*)&compSize, sizeof(int))) {}
+   
+    void searchInfo(firm* Comp, int* size, double ave) {
+        int sum = 0;
+        for (size_t i = 0; i < *size; i++)
+        {
+            if ((Comp + i)->compSize >= 19)
+                sum += (Comp + i)->compPrice;
+        }
+        for (size_t i = 0; i < *size; i++)
+        {
+            if (sum > 0)
+                ave = sum / *size;
+        }
+        for (size_t i = 0; i < *size; i++)
+        {
+            if ((Comp + i)->compSize >= 19) {
+                (Comp + i)->outInfo();
+            }
+        }
+       // fwrite(comp, *size, ave, file_out);
+        
+    }
 
-    fileStrmIn.close();
-}
-/*Ввести в Memo или в StringGrid некоторое количество символов русского 
-алфавита и записать их в бинарный файл. Переписать бинарный файл так,
-чтобы все символы были записаны прописными буквами. Распечатать
-символы до и после изменения данных в файле.*/
-void lvl2() {
+    /*создать бинарный файл с информацией о предложенных к реализации мониторах:
+    - название фирмы;
+    - размер по диагонали;
+    - стоимость;
+    Определить среднюю цену мониторов, размером не менее 19 дюймов и распечатать сведения о них.*/
 
-}
-/*Создать бинарный файл, компонентами которого является структура,
-содержащая следующие поля:
-- фамилия и инициалы покупателя;
-- дата покупки4
-- общая стоимость приобретенного товара;
-- начальный процент скидки на последующие приобретаемые товары.
-Переписать бинарный файл так, чтобы процентная скидка была бы
-увеличена на 5%, если покупатель заплатил за предыдущий товар 5000 грн.*/
-void lvl3() {
+    void lvl1() {
+        FILE* file_out, * file;
+        int ave = 0;
+        int size = 0;
+        file = fopen("D:\\Computers.bin", "wb");
+        cout << "Enter quantity of products: ";
+        while (!(cin >> size))
+        {
+            cout << "Wrong input" << endl;
+            cin.clear();
+            cin.ignore(65535, '\n');
+        }
+        int* tempSize = &size;
+        firm* Comp = new firm[size];
+        delete[]Comp;
+        initInfo(Comp, tempSize);      
+        showInfo(Comp, tempSize, file);
+        fclose(file);
+        firm* comp = new firm[size];
+        file_out = fopen("D:\\Computers.bin", "rb");
+        fread(comp,sizeof(firm),size, file_out);
+        searchInfo(comp, tempSize, ave);
 
-}
-int main()
-{
-    lvl1();
-}
+        fclose(file_out);
+    }
+    /*Ввести в Memo или в StringGrid некоторое количество символов русского
+    алфавита и записать их в бинарный файл. Переписать бинарный файл так,
+    чтобы все символы были записаны прописными буквами. Распечатать
+    символы до и после изменения данных в файле.*/
+    void lvl2() {
 
+    }
+    /*Создать бинарный файл, компонентами которого является структура,
+    содержащая следующие поля:
+    - фамилия и инициалы покупателя;
+    - дата покупки;
+    - общая стоимость приобретенного товара;
+    - начальный процент скидки на последующие приобретаемые товары.
+    Переписать бинарный файл так, чтобы процентная скидка была бы
+    увеличена на 5%, если покупатель заплатил за предыдущий товар 5000 грн.*/
+    void lvl3() {
+
+    }
+    int main()
+    {
+        lvl1();
+    }
